b5b044cfed82e4df752a746ad4722b4afcb8b658
Franklin Aurelio <franklin.carvalho@unifesp.br>
2022-12-28 11:33:12 -0300

    feat: header

--- travel_app/travel_app/View/Header/HomeTableViewHeader.swift
+++ travel_app/travel_app/View/Header/HomeTableViewHeader.swift
@@ -16,7 +16,14 @@ class HomeTableViewHeader: UIView {
     
     @IBOutlet weak var bannerImageView: UIImageView!
     
+    @IBOutlet weak var bannerView: UIView!
     func configView(){
         headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 500
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
     }
 }
