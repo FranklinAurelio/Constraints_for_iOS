a34e620527e328c17727e71bd2cab3586873743f
Franklin Aurelio <franklin.carvalho@unifesp.br>
2023-01-26 09:41:29 -0300

    feat: using scrollView with stackView and constraints

---
+++ travel App/AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetalheViewController.swift
+//  AluraViagens
+//
+//  Created by Franklin Carvalho on 26/01/23.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
--- travel App/AluraViagens/View/ViewController.swift
+++ travel App/AluraViagens/View/ViewController.swift
@@ -68,6 +68,11 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detalheController = DetalheViewController(nibName: "DetalheViewController", bundle: nil)
+        navigationController?.pushViewController(detalheController, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
             let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self, options: nil)?.first as? HomeTableViewHeader
