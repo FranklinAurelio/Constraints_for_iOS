3d753b01d6600b304b203144a92e9b78bcaefa3c
Franklin Aurelio <franklin.carvalho@unifesp.br>
2023-01-26 11:54:58 -0300

    chore: layout with stackView inside other StackView

--- travel App/AluraViagens/View/DetalheViewController/DetalheViewController.swift
+++ travel App/AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class DetalheViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var viagemImage: UIImageView!
+    
+    @IBOutlet weak var tituloViagemLabel: UILabel!
+    
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    // MARK: - Actions
 
 
-
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
